28e47122ab13a66b0f2c713a1f4479376138c6ac
Balázs Édes <edesbalazs@gmail.com>
2022-02-25 14:25:41 +0100

    added generated schemas

---
+++ src/generate.ts
@@ -0,0 +1,26 @@
+import { promises as fs } from "fs";
+import { resolve } from "path";
+import { OpenAPIObject } from "@oats-ts/openapi-model";
+import { generateBodiesOpenApiObject } from "./bodies/generateBodiesOpenApiObject";
+import { generateHttpMethodsOpenApiObject } from "./methods/generateHttpMethodsOpenApiObject";
+import { generateParametersOpenApiObject } from "./parameters/generateParametersOpenApiObject";
+
+const schemas: [string, () => OpenAPIObject][] = [
+  ["generated-schemas/parameters.json", generateParametersOpenApiObject],
+  ["generated-schemas/methods.json", generateHttpMethodsOpenApiObject],
+  ["generated-schemas/bodies.json", generateBodiesOpenApiObject],
+];
+
+async function generateSchemas() {
+  for (const [schemaPath, createSchema] of schemas) {
+    await fs.writeFile(
+      resolve(schemaPath),
+      JSON.stringify(createSchema(), null, 2),
+      {
+        encoding: "utf-8",
+      }
+    );
+  }
+}
+
+generateSchemas();
